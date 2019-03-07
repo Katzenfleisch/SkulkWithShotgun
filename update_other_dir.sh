15ce6d99363a47b98f0500958635d31d46a1fc48
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2019-03-07 20:33:23 +0100

    Comming workshop version of SkulkWithShotgun v325

---
+++ update_other_dir.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+## \file update_other_dir.sh
+## 
+## \author Jean-Baptiste Laurent
+## \<jeanbaptiste.laurent.pro@gmail.com\>
+## 
+## \date Started on  Sat Apr  4 15:42:49 2015 Jean-Baptiste Laurent
+## \date Last update Thu Mar  7 20:27:43 2019 Jean-Baptiste Laurent
+## \brief File description to place here
+##
+
+mod_name="SkulkWithShotgun"
+base_dir="/media/NS2"
+if [ $# -eq 1 ]
+then
+    # rm -vrf $base_dir${mod_name}/*
+    # rm -rf $base_dir${mod_name}_production/output/
+    # mkdir -p $base_dir${mod_name}_production/output/
+    # echo "Remove done"
+    ##
+    rsync --delete --progress -vcr ./* $base_dir/${mod_name}_production/output/
+
+    cp .modinfo $base_dir/${mod_name}_production/output/
+fi
+
+rsync --delete --progress -vcr ../${mod_name}/ $base_dir/${mod_name}/
+echo "Copy done"
+
+# if [ $# -eq 1 ]
+# then
+#     rm -vrf $base_dir${mod_name}/*
+#     echo "Remove done"
+# fi
+# cp -vru ../${mod_name}/ $base_dir
+# echo "Copy done"
